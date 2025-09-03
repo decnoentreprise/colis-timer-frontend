23dd581e0c8308381c40fec175691b920ae9c031
decnoentreprise <decnoentreprise@gmail.com>
2025-09-03 19:50:08 +0200

    Initial commit frontend

---
+++ sw.js
@@ -0,0 +1,17 @@
+// Cache-first service worker for offline support
+const CACHE = 'colis-timer-v7';
+const ASSETS = [
+  './','./index.html','./manifest.webmanifest','./sw.js',
+  './icons/icon-192.png','./icons/icon-512.png'
+];
+self.addEventListener('install', (e)=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(caches.match(e.request).then(res=> res || fetch(e.request)));
+});
